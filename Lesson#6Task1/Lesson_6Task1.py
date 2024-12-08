582122e7e00ccb5d2d9afc32787d846695e7e3e5
maxstar <maksimgavura78@gmail.com>
2024-12-09 00:08:12 +0200

    Добавьте файлы проекта.

---
+++ Lesson#6Task1/Lesson_6Task1.py
@@ -0,0 +1,7 @@
+﻿N = int(input('Vvedite koli4estvo 4isel: '))
+count_null = 0
+for i in range(N):
+   P = int(input('ccedite celoe 4islo: '))
+   if P == 0:
+       count_null += 1
+print(f"koli4estvo nulley= {count_null}")
